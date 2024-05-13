db7fe86cea0a941bec152131ed81344de46ed0dc
Rana Umair <rjee4631@gmail.com>
2024-05-13 11:42:06 +0500

    Create html example.php

---
+++ html example.php
@@ -0,0 +1,9 @@
+<div class="custom-email-field flex-col mb-[16px]">
+    <label for="message-text" class="col-form-label ">{{__('Detail')}} <span class="text-[#D80F04] ml-[4px]">*</span> </label>
+    <div class="eg-main-cont ">
+        <textarea class="form-control eg-customize-inp bg-white my_textarea input-field-styling"
+                  id="message-text" name="message" rows="4" required
+                  maxlength="90" placeholder="45 Korean characters, 90 English characters or less"></textarea>
+    </div>
+    <p class="text-[14px] leading-[20px] tracking-[-0.2px] font-normal text-[#999999] text-right mt-[4px]" id="message-text-count">0/90</p>
+</div>
